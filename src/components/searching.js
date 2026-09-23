a094d72ede503cb75031970c99939179b00f2fe1
KurakinPavel <pu.kurakin@yandex.ru>
2026-09-23 16:47:03 +0300

    without search

--- src/components/searching.js
+++ src/components/searching.js
@@ -5,6 +5,6 @@ export function initSearching(searchField) {
 	const compare = createComparison(defaultRules.skipEmptyTargetValues, rules.searchMultipleFields(searchField, ['date', 'customer', 'seller'], false));
 
 	return (data, state, action) => {
-		return data.search(field => compare(field, searchField));
+		return data.search(row => compare(row, state));
 	};
 }
